205b273a0c89c03ff7efc0061c60daa1972d7afb
DennisGabriel <dennisgabr745@gmail.com>
2024-11-15 01:55:08 -0300

    adicionando modulos e separando por arquivos

--- primeira_questao/primeira.py
+++ primeira_questao/primeira.py
@@ -1,4 +1,5 @@
 import math
+import funcoes
 
 areas = [
     [300, 400, 14],
@@ -12,86 +13,53 @@ areas = [
     [1100, 1200, 6]
 ]
 
-def media(areas):
-    media = 0
-    ponto_medio = 0
-    k = 0 
-    for area in areas:
-        ponto_medio = (area[0] + area[1])/2
-        media += ponto_medio * area[2]
-        k += area[2]
 
-    media = media / k
+###### ITEM 1 ######
 
-    print('média =', media)
-    return media
+# media
+media = funcoes.media(areas)
+# fim media
 
-def moda(anterior, moda, posterior):
-    moda = moda[0] + (posterior[2] / (posterior[2] + anterior[2])) * (moda[1] - moda[0])
-    print('moda =', round(moda, 2))
+# moda
+funcoes.moda(areas[2], areas[3], areas[4])
+# fim moda
 
-def mediana(frequencias, metade_total, frequencia_anterior):
-    h = (frequencias[-1][1] - frequencias[-1][0])
-    mediana = frequencias[-1][0] + (( metade_total - frequencia_ant ) / frequencias[-1][2]) * h
-    print('mediana =', round(mediana, 2))
+# mediana 
+soma_total = funcoes.somatorio(areas)
+posicao_ideal = funcoes.posicao_ideal(areas, soma_total, 0.5)
+funcoes.encontra_medidas(posicao_ideal, soma_total, 0.5, "MEDIANA")
+# fim mediana
 
-def somatorio(areas):
-    soma_total = 0
-    for area in areas:
-        soma_total += area[2]
-    return soma_total
-
-
-def frequencia_anterior(areas, posicao_limite):
-    somatorio_frequencia = 0
-    for i in range(posicao_limite):
-        somatorio_frequencia += areas[i][2]
-    return somatorio_frequencia
+###### FIM DO ITEM 1 ######
 
 
 
-## item 1
-
-## MEDIA
-media = media(areas)
-## FIM MEDIA
-
-## MODA
-moda(areas[2], areas[3], areas[4])
-## FIM MODA
-
-## MEDIANA 
-soma_total = somatorio(areas)
-metade_total = soma_total / 2
-frequencia_ant = frequencia_anterior(areas, 4)
-mediana([areas[0], areas[1], areas[2], areas[3], areas[4]], metade_total, frequencia_ant)
-## FIM MEDIANA
-
-## fim do item 1
-
-## item 2
-def desvio_padrao(areas, media):
-    variancia = 0
-    k = 0 
-    for area in areas:
-        ponto_medio = (area[0] + area[1]) / 2
-        variancia += ( ( ponto_medio - media ) ** 2 ) * area[2]
-        k += area[2]
-
-    variancia = variancia / ( k - 1 )
-    print('desvio padrão =', round(math.sqrt(variancia), 2))
+###### ITEM 2 ######
+funcoes.desvio_padrao(areas, media)
+###### FIM DO ITEM 2 ######
 
 
-desvio_padrao(areas, media)
-## fim do item 2
 
 
-## item 3
-def q1(area, soma_total, frequencia_anterior):
-    valor_temp = soma_total * 0.25
-    h = (area[1] - area[0])
-    q1 = area[0] + (valor_temp - frequencia_anterior) / area[2] * h
-    print("Q1 =", round(q1, 2))
+###### ITEM 3 ######
+# Q1
+posicao_ideal = funcoes.posicao_ideal(areas, soma_total, 0.25)
+funcoes.encontra_medidas(posicao_ideal, soma_total, 0.25, "Q1")
+# FIM Q1
 
-frequencia_ant = frequencia_anterior(areas, 2)
-q1(areas[2], soma_total, frequencia_ant)
+# D3
+posicao_ideal = funcoes.posicao_ideal(areas, soma_total, 0.3)
+funcoes.encontra_medidas(posicao_ideal, soma_total, 0.3, "D3")
+# fim D3
+
+# D7
+posicao_ideal = funcoes.posicao_ideal(areas, soma_total, 0.7)
+funcoes.encontra_medidas(posicao_ideal, soma_total, 0.7, "D7")
+# fim D7
+
+# D7
+posicao_ideal = funcoes.posicao_ideal(areas, soma_total, 0.7)
+funcoes.encontra_medidas(posicao_ideal, soma_total, 0.7, "D7")
+# fim D7
+
+###### FIM DO ITEM 3 ######
